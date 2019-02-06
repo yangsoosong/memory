e377f4dc30e113127f2ac830d84843526e43e069
yangsoosong <song.yang@husky.neu.edu>
2019-02-05 22:42:43 -0500

    Forgot to push

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -9,39 +9,170 @@ export default function game_init(root) {
 class Starter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { left: false };
-  }
-
-  swap(_ev) {
-    let state1 = _.assign({}, this.state, { left: !this.state.left });
-    this.setState(state1);
-  }
-
-  hax(_ev) {
-    alert("hax!");
+	  this.tiles = 
+		 [
+		  {value: "A", chekced: "false"},
+		  {value: "B", checked: "false"},
+		  {value: "C", checked: "false"},
+		  {value: "D", checked: "false"},
+		  {value: "E", checked: "false"},
+		  {value: "F", checked: "false"},
+		  {value: "G", checked: "false"},
+		  {value: "H", checked: "false"},
+		  {value: "A", checked: "false"},
+		  {value: "B", checked: "false"},
+		  {value: "C", checked: "false"},
+		  {value: "D", checked: "false"},
+		  {value: "E", checked: "false"},
+		  {value: "F", checked: "false"},
+		  {value: "G", checked: "false"},
+		  {value: "H", checked: "false"}
+		 ];
+	  const shuffleTiles = _.shuffle(this.tiles);
+	  this.timer = null;
+	  this.state = {
+		  clicks: 0,
+		  tiles: shuffleTiles,
+		  lastClicked: -1,
+	  };
   }
 
   render() {
-    let button = <div className="column" onMouseMove={this.swap.bind(this)}>
-      <p><button onClick={this.hax.bind(this)}>Click Me</button></p>
-    </div>;
+	  let row1 = _.map(this.state.tiles, (tile, ii) => {
+		  if (ii < 4) {
+			  return <DisplayTile tile={tile} root={this} key={ii} ii={ii} />
+		  }
+	  });
 
-    let blank = <div className="column">
-      <p>Nothing here.</p>
-    </div>;
+	  let row2 = _.map(this.state.tiles, (tile, ii) => {
+		  if (ii >= 4 && ii < 8) {
+			  return <DisplayTile tile={tile} root={this} key={ii} ii={ii} />
+		  }
+	  });
+  	  let row3 = _.map(this.state.tiles, (tile, ii) => {
+		  if (ii >= 8 && ii < 12) {
+			  return <DisplayTile tile={tile} root={this} key={ii} ii={ii} />
+		  }
+	  });
+	  let row4 = _.map(this.state.tiles, (tile, ii) => {
+		  if (ii >= 12 && ii < 16) {
+			  return <DisplayTile tile={tile} root={this} key={ii} ii={ii} />
+		  }
+	  });
 
-    if (this.state.left) {
-      return <div className="row">
-        {button}
-        {blank}
-      </div>;
-    }
-    else {
-      return <div className="row">
-        {blank}
-        {button}
-      </div>;
-    }
+	  let resetButton = <p><button onClick={() => this.reset_memory()}> Reset the Game</button></p>
+
+	  return(
+		  <div className="container">
+
+		  <div className="row">
+		  {row1}
+		  </div>
+		  <div className="row">
+		  {row2}
+		  </div>
+		  <div className="row">
+		  {row3}
+		  </div>
+		  <div className="row">
+		  {row4}
+		  </div>
+
+		  <div className="row">
+		  <p>Clicks you made so far: {this.state.clicks}</p>
+		  </div>
+
+		  {resetButton}
+
+		  </div>
+	  );
   }
+ 
+ is_checked(ii) {
+	let xx = this.state.tiles;
+	let lc = this.state.lastClicked;
+	if (xx[ii].value != xx[lc].value) {
+		let yy = _.map(this.state.tiles, (tile, jj) => {
+			if (ii == jj || lc == jj) {
+				return _.assign({}, tile, {completed: false});
+			}
+			else {
+				return tile;
+			}
+		});
+		this.setState(_.assign({}, this.state, {tiles: yy}, {lastClicked: -1}));
+	}
+	else {
+		this.setState(_.assign({}, this.state, {tiles: xx}, {lastClicked: -1}));
+	}
+	this.timer = null;
+}
+
+
+
+ tile_click(ii) {
+	if (this.timer == null) {
+		let xx = _.map(this.state.tiles, (tile, jj) =>  {
+			if (ii == jj) {
+				return _.assign({}, tile, {completed: true});
+			}
+			else {
+				return tile;
+			}
+		});
+
+		if (this.state.lastClicked == -1) {
+			this.setState(_.assign({}, this.state, {tiles: xx}, {clicks: this.state.clicks + 1}, {lastClicked: ii}));
+		}
+		else {
+			this.setState(_.assign({}, this.state, {tiles: xx}, {clicks: this.state.clicks + 1}));
+			this.timer = setTimeout((function() {this.is_checked(ii)}).bind(this), 1000);
+		}
+	}
+}
+
+reset_memory() {
+	const shuffleTiles = _.shuffle(this.tiles);
+	this.setState(
+		{ tiles: shuffleTiles,
+			timer: null,
+			clicks: 0,
+			lastClicked: -1,
+		}
+	);
+
+}
+}
+/**
+ * Got the shuffle code from:
+ * https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
+function shuffleArray(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+function DisplayTile(props) {
+	let {tile, root, ii} = props
+
+	if (tile.completed) {
+		return <div className="column">
+			<p><button>{tile.value}</button></p>
+			</div>
+	}
+
+	else {
+		return <div className="column">
+			<p><button onClick={() => root.tile_click(ii)}></button></p>
+			</div>
+	}
 }
 
